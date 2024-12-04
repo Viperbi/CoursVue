be4b8acd2bd39a35686208e7ade079d83137fe43
Viperbi <nickels.florian@gmail.com>
2024-12-04 10:20:13 +0100

    fixed rooter

--- src/components/shared/NavBar.vue
+++ src/components/shared/NavBar.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>
-    <input v-model="value" />
-  </div>
+	<div>
+		<input v-model="value" />
+
+		<a class="navbar-brand" href="#">Always expand</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-toggle="collapse"
+			data-target="#navbarsExample02"
+			aria-controls="navbarsExample02"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarsExample02">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#"
+						>Home <span class="sr-only">(current)</span></a
+					>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-md-0">
+				<input class="form-control" type="text" placeholder="Search" />
+			</form>
+		</div>
+	</div>
 </template>
 
-<script setup lang='js'>
+<script setup lang="js">
 import { computed, watch, onMounted, onUpdated, onBeforeUnmount } from 'vue'
 
 const props = defineProps({
@@ -46,8 +75,6 @@ onUpdated(() => {
 onBeforeUnmount(() => {
   stopWatch();
 });
-
 </script>
 
-<style scoped lang="css">
-</style>
+<style scoped lang="css"></style>
